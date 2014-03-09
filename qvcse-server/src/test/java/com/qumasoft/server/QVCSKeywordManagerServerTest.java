ba5679faaa47dad35d2b7e7d0c3b55a3d8076959
Jim Voris <jimv@comcast.net>
2014-03-08 20:46:45 -0500

    Keyword manager tests need to run in order.

--- qvcse-server/src/test/java/com/qumasoft/server/QVCSKeywordManagerServerTest.java
+++ qvcse-server/src/test/java/com/qumasoft/server/QVCSKeywordManagerServerTest.java
@@ -1,17 +1,17 @@
-//   Copyright 2004-2014 Jim Voris
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
+/*   Copyright 2004-2014 Jim Voris
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.qumasoft.server;
 
 import com.qumasoft.TestHelper;
@@ -28,10 +28,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
-import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.fail;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -51,18 +49,20 @@ public class QVCSKeywordManagerServerTest {
         TestHelper.stopServer();
     }
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() {
+    @Test
+    public void testRunInOrder() {
+        testExpandKeywords();
+        testExpandKeywordsNonTip();
+        testExpandKeywordsBranchTip();
+        testExpandKeywordsBranchNonTip();
+        testBinaryExpandKeywords();
+        testContractKeywords();
+        testContractKeywordsNoKeywords();
     }
 
     /**
      * Test of expandKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testExpandKeywords() {
         System.out.println("testExpandKeywords");
 
@@ -112,7 +112,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of expandKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testExpandKeywordsNonTip() {
         System.out.println("testExpandKeywordsNonTip");
 
@@ -162,7 +161,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of expandKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testExpandKeywordsBranchTip() {
         System.out.println("testExpandKeywordsBranchTip");
 
@@ -212,7 +210,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of expandKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testExpandKeywordsBranchNonTip() {
         System.out.println("testExpandKeywordsBranchNonTip");
 
@@ -262,7 +259,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of testBinaryExpandKeywords
      */
-    @Test
     public void testBinaryExpandKeywords() {
         System.out.println("testBinaryExpandKeywords");
 
@@ -312,7 +308,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of contractKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testContractKeywords() {
         System.out.println("testContractKeywords");
 
@@ -356,7 +351,6 @@ public class QVCSKeywordManagerServerTest {
     /**
      * Test of contractKeywords method, of class com.qumasoft.qvcslib.QVCSKeywordManager.
      */
-    @Test
     public void testContractKeywordsNoKeywords() {
         System.out.println("testContractKeywordsNoKeywords");
 
