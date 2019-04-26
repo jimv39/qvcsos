1b3a6baf2cb39f9dbc5d0931942012d074ff7cd5
Jim Voris <jimv@comcast.net>
2019-04-26 10:48:59 -0400

    Standardize file copyright header.

--- qvcse-qvcslib/src/main/java/com/qumasoft/qvcslib/Utility.java
+++ qvcse-qvcslib/src/main/java/com/qumasoft/qvcslib/Utility.java
@@ -1,6 +1,5 @@
-package com.qumasoft.qvcslib;
 /*
- * Copyright 2014-2015 JimVoris.
+ * Copyright 2014-2019 JimVoris.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +13,7 @@ package com.qumasoft.qvcslib;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.qumasoft.qvcslib;
 
 import java.io.File;
 import java.io.FileInputStream;
